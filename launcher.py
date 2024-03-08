d3edcae2ad746ece2a7c2c0ab2e854231a271f94
ples-ovidiu-30126 <ovidiu.ples23@gmail.com>
2024-03-08 18:18:07 +0200

    eliminate suv class
    add van, bicycle classes
    
    train more models

--- launcher.py
+++ launcher.py
@@ -1,52 +1,44 @@
-import os
-
-import cv2
-
-from training.training import Training
-from ultralytics import YOLO
-
-
 def launcher():
     # training
+    pass
     # model = YOLO("yolov8n.yaml")
-    # model.train(data="config.yaml", epochs=70)
+    # model.train(data=r"source/config.yaml", epochs=10, name='moto_train')
 
     # prediction
-    VIDEOS_DIR = r'C:\Licenta\inregistrari'
-
-    video_path = os.path.join(VIDEOS_DIR, 'vlc-record-2024-02-13-14h14m13s-rtsp___192.168.82.149_live-.mp4')
-    video_path_out = '{}_out.mp4'.format(video_path)
-
-    cap = cv2.VideoCapture(video_path)
-    ret, frame = cap.read()
-    H, W, _ = frame.shape
-    out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
-
-    model_path = os.path.join('.', 'runs', 'detect', 'train5', 'weights', 'last.pt')
-
-    # Load a model
-    model = YOLO(model_path)
-
-    threshold = 0.1
-
-    while ret:
-
-        results = model(frame)[0]
-
-        for result in results.boxes.data.tolist():
-            x1, y1, x2, y2, score, class_id = result
-
-            if score > threshold:
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-                cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
-
-        out.write(frame)
-        ret, frame = cap.read()
-
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+    # VIDEOS_DIR = r'C:\Licenta\raw_data\videos'
+    #
+    # video_path = os.path.join(VIDEOS_DIR, 'video_5_night.mp4')
+    # video_path_out = '{}_processed.mp4'.format(video_path)
+    #
+    # cap = cv2.VideoCapture(video_path)
+    # ret, frame = cap.read()
+    # H, W, _ = frame.shape
+    # out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+    #
+    # model_path = os.path.join('.', 'runs', 'detect', 'train6', 'weights', 'last.pt')
+    #
+    # # Load a model
+    # model = YOLO(model_path)
+    #
+    # threshold = 0.2
+    #
+    # while ret:
+    #
+    #     results = model(frame)[0]
+    #     for result in results.boxes.data.tolist():
+    #         x1, y1, x2, y2, score, class_id = result
+    #
+    #         if score > threshold:
+    #             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), colors[class_id], 4)
+    #             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
+    #                         cv2.FONT_HERSHEY_SIMPLEX, 1.3, colors[class_id], 3, cv2.LINE_AA)
+    #
+    #     out.write(frame)
+    #     ret, frame = cap.read()
+    #
+    # cap.release()
+    # out.release()
+    # cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
---
+++ source/training/data_factory.py
@@ -0,0 +1,59 @@
+import os
+
+import cv2
+
+
+class DataFactory:
+    @staticmethod
+    def modify_class_labels(dir_path, old_class, new_class):
+        for filename in os.listdir(dir_path):
+            if filename.endswith('.txt'):
+                filepath = os.path.join(dir_path, filename)
+                with open(filepath, 'r') as file:
+                    lines = file.readlines()
+
+                modified_lines = []
+                for line in lines:
+                    if line.startswith(str(old_class)):
+                        modified_line = str(new_class) + line[1:]
+                        modified_lines.append(modified_line)
+                    else:
+                        modified_lines.append(line)
+
+                with open(filepath, 'w') as file:
+                    file.writelines(modified_lines)
+        print("class " + str(old_class) + " modified to " + str(new_class))
+
+    @staticmethod
+    def video_to_frames(video_path, output_path, n=4):
+        video_name = video_path.split("\\")[-1]
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        cap = cv2.VideoCapture(video_path)
+
+        frame_idx = 0
+        saved_frame_idx = 0
+
+        while True:
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            if frame_idx % n == 0:
+                frame_file_path = os.path.join(output_path, f"{video_name}_frame_{saved_frame_idx}.jpg")
+                cv2.imwrite(frame_file_path, frame)
+                print(f"Saved {frame_file_path}")
+                saved_frame_idx += 1
+
+            frame_idx += 1
+
+        cap.release()
+        print("Done saving frames.")
+
+# DataFactory.video_to_frames(video_path=r'C:\Licenta\raw_data\videos\video_9.mp4',
+#                          output_path=r'C:\Licenta\raw_data\images\video_9',
+#                          n=10)
+
+# DataFactory.modify_class_labels(r"C:\Licenta\raw_data\yolo_labels\obj_train_data_video_5", 6, 5)
