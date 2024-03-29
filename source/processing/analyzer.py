38e7d9aeaeb8ad4b1f7e4ad164edc5a7696f725c
ples-ovidiu-30126 <ovidiu.ples23@gmail.com>
2024-03-29 16:29:54 +0200

    clean-up analyzer

--- source/processing/analyzer.py
+++ source/processing/analyzer.py
@@ -9,54 +9,26 @@ from source.variables.colors import class_colors
 
 
 class Analyzer:
+    def __init__(self):
+        # model_path = os.path.join('.', '.', 'runs', 'detect', 'train15', 'weights', 'last.pt')
+        self.model = YOLO("yolov8n.pt")
+        self.model_confidence = 0.5
+        self.current_frame = 0
+        self.previous_vehicles = []
+
     def process_video(self, input_path, output_path=None):
-        if output_path is None:  # same dir as input
+        if output_path is None:
             path = input_path.split(".mp4")[0:-1]
-            output_path = "\\".join(path) + "_processed.mp4"
+            output_path = "\\".join(path) + "_processed.mp4"  # same dir as input
 
         cap = cv2.VideoCapture(input_path)
         ret, frame = cap.read()
-        H, W, _ = frame.shape
-        out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
+        h, w, _ = frame.shape
+        out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (w, h))
 
-        #model_path = os.path.join('.', 'runs', 'detect', 'train15', 'weights', 'last.pt')
-        model = YOLO("yolov8n.pt")
-
-        vehicle_id = 0
-        unassigned_id = 0
-        previous_vehicles = []
-        current_frame = 0
-        threshold = 0.5
         while ret:
-            results = model(frame)[0]
-
-            for result in results.boxes.data.tolist():
-                x1, y1, x2, y2, score, class_id = result
-                if current_frame > 0:
-                    max_iou = 0
-                    matching_vehicle = None
-                    for vehicle in previous_vehicles:
-                        iou = self.calculate_iou([vehicle.box[0], vehicle.box[1], vehicle.box[2], vehicle.box[3]], [x1, y1, x2, y2])
-                        if iou > max_iou:
-                            max_iou = iou
-                            matching_vehicle = vehicle
-                    if max_iou > 0.4:
-                        vehicle_id = matching_vehicle.id
-                        previous_vehicles.remove(matching_vehicle)
-                        previous_vehicles.append(Vehicle(vehicle_id, [x1, y1, x2, y2]))
-                    else:
-                        vehicle_id = random.randint(1, 1000)
-                        previous_vehicles.append(Vehicle(vehicle_id, [x1, y1, x2, y2]))
-                else:
-                    vehicle_id = random.randint(1, 1000)
-                    previous_vehicles.append(Vehicle(id=vehicle_id, box=[x1, y1, x2, y2]))
-
-                if score > threshold:
-                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), class_colors[0], 4)
-                    cv2.putText(frame, results.names[int(class_id)].upper() + " ID: " + str(vehicle_id), (int(x1), int(y1 - 10)),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1.3, class_colors[0], 3, cv2.LINE_AA)
-
-            current_frame += 1
+            frame = self.process_frame(frame)
+            self.current_frame += 1
             out.write(frame)
             ret, frame = cap.read()
 
@@ -67,56 +39,51 @@ class Analyzer:
     def process_stream(self, input_path):
         cap = cv2.VideoCapture(input_path)
 
-        # model_path = os.path.join('.', 'runs', 'detect', 'train15', 'weights', 'last.pt')
-        model = YOLO("yolov8n.pt")
-
-        vehicle_id = 0
-        unassigned_id = 0
-        previous_vehicles = []
-        current_frame = 0
-        threshold = 0.5
         while True:
             ret, frame = cap.read()
             if frame is None:
                 break
 
-            results = model(frame)[0]
-
-            for result in results.boxes.data.tolist():
-                x1, y1, x2, y2, score, class_id = result
-                if current_frame > 0:
-                    max_iou = 0
-                    matching_vehicle = None
-                    for vehicle in previous_vehicles:
-                        iou = self.calculate_iou([vehicle.box[0], vehicle.box[1], vehicle.box[2], vehicle.box[3]], [x1, y1, x2, y2])
-                        if iou > max_iou:
-                            max_iou = iou
-                            matching_vehicle = vehicle
-                    if max_iou > 0.4:
-                        vehicle_id = matching_vehicle.id
-                        previous_vehicles.remove(matching_vehicle)
-                        previous_vehicles.append(Vehicle(vehicle_id, [x1, y1, x2, y2]))
-                    else:
-                        vehicle_id = random.randint(1, 1000)
-                        previous_vehicles.append(Vehicle(vehicle_id, [x1, y1, x2, y2]))
-                else:
-                    vehicle_id = random.randint(1, 1000)
-                    previous_vehicles.append(Vehicle(id=vehicle_id, box=[x1, y1, x2, y2]))
-
-                if score > threshold:
-                    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), class_colors[0], 4)
-                    cv2.putText(frame, results.names[int(class_id)].upper() + " ID: " + str(vehicle_id), (int(x1), int(y1 - 10)),
-                                cv2.FONT_HERSHEY_SIMPLEX, 1.3, class_colors[0], 3, cv2.LINE_AA)
-
+            frame = self.process_frame(frame)
             frame = cv2.resize(frame, (1200, 700))
             cv2.imshow('Stream', frame)
-            current_frame += 1
+            self.current_frame += 1
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         cap.release()
         cv2.destroyAllWindows()
+
+    def process_frame(self, frame):
+        results = self.model(frame)[0]
+
+        for result in results.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = result
+            if self.current_frame > 0:
+                max_iou = 0
+                matching_vehicle = None
+                for vehicle in self.previous_vehicles:
+                    iou = self.calculate_iou([vehicle.box[0], vehicle.box[1], vehicle.box[2], vehicle.box[3]], [x1, y1, x2, y2])
+                    if iou > max_iou:
+                        max_iou = iou
+                        matching_vehicle = vehicle
+                if max_iou > 0.4:
+                    vehicle_id = matching_vehicle.id
+                    self.previous_vehicles.remove(matching_vehicle)
+                    self.previous_vehicles.append(Vehicle(vehicle_id, [x1, y1, x2, y2]))
+                else:
+                    vehicle_id = random.randint(1, 1000)
+                    self.previous_vehicles.append(Vehicle(vehicle_id, [x1, y1, x2, y2]))
+            else:
+                vehicle_id = random.randint(1, 1000)
+                self.previous_vehicles.append(Vehicle(id=vehicle_id, box=[x1, y1, x2, y2]))
+
+            if score > self.model_confidence:
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), class_colors[0], 4)
+                cv2.putText(frame, results.names[int(class_id)].upper() + " ID: " + str(vehicle_id), (int(x1), int(y1 - 10)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.3, class_colors[0], 3, cv2.LINE_AA)
+        return frame
 
     @staticmethod
     def calculate_iou(box1, box2):
