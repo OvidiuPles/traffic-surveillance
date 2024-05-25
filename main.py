a91b3085ce403f622bb5745390f29755c256dbc7
ples-ovidiu-30126 <ovidiu.ples23@gmail.com>
2024-05-25 17:34:00 +0300

    add freeze support

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import multiprocessing
+
 import pyqtgraph as pg
 
 from source.gui.main_gui import MainGUI
 
-if __name__ == "__main__":
+
+def main_app():
     app = pg.mkQApp()
     window = MainGUI()
     window.show()
     app.exec()
+
+
+if __name__ == "__main__":
+    # first two predictions of the yolo vehicles model open a new gui when running in exe, original gui is not responding until the second is closed
+    # freeze support is a fix for now, takes longer for the first predictions to execute but then works fine, also works fine as a python script
+    # https://github.com/ultralytics/ultralytics/issues/3997
+    multiprocessing.freeze_support()
+    process = multiprocessing.Process(target=main_app)
+    process.start()
+    process.join()
